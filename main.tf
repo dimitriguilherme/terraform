9ca0e0446f8fbbca8b3f7918869e98b06c86e085
dimitrik <dimitri.net@gmail.com>
2019-12-12 09:14:48 -0300

    test5

---
+++ main.tf
@@ -0,0 +1,21 @@
+#aws provider
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = ".aws/credentials"
+  profile                 = "terra"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket129vc"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket12344"
+    Environment = "Dev444"
+  }
+}
+resource "aws_s3_bucket_object" "object" {
+  bucket = "${aws_s3_bucket.b.id}"
+  key = "oioioi.txt"
+  source = "arquivo.txt"
+}
