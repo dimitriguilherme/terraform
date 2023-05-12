cb17fb92cc3d474d66e77cb193af47ca3cf3ac00
Dimitri Guilherme <dimitri.net@gmail.com>
2023-05-11 22:12:17 -0300

    Add files via upload
    add template 1

---
+++ 1/a.tf
@@ -0,0 +1,32 @@
+# # terraform {
+# #   required_providers {
+# #     aws = {
+# #       source  = "hashicorp/aws"
+# #       version = "~> 4.0"
+# #     }
+# #   }
+# # }
+
+# # # Configure the AWS Provider
+# # provider "aws" {
+# #   region = "us-east-1"
+# # }
+
+
+
+# # Create a VPC
+# resource "aws_vpc" "example" {
+#   cidr_block = "10.0.0.0/16"
+# }
+# Terraform 0.12 and earlier:
+
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 4.0"
+  region  = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
